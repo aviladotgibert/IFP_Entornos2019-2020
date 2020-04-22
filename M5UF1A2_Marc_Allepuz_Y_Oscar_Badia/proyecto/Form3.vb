84c6e9c354390000400a3857df680f49f108797f
Alex Viladot Gibert <63657502+aviladotgibert@users.noreply.github.com>
2020-04-22 09:52:02 +0200

    Uploading files
    Es posible que después en el merge nos de error ya que es un proyecto ya subido

---
+++ M5UF1A2_Marc_Allepuz_Y_Oscar_Badia/proyecto/Form3.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form3
+    Dim Operacion As String
+    Dim ValorResultado As Nullable(Of Double
+        ) = Nothing
+    Dim Valor2 As Nullable(Of Double
+        ) = Nothing
+    Dim SePresionaOperador As Boolean
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "0"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "1"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        EvaluaRestriccionesParaConcatenar()
+        TextResultado.Text &= "9"
+    End Sub
+
+    Private Sub Buttonp_Click(sender As Object, e As EventArgs) Handles Buttonp.Click
+
+        If InStr(TextResultado.Text, ".", CompareMethod.Text) = 0 Then
+            TextResultado.Text &= "."
+        End If
+    End Sub
+    Private Sub AreaCuadrado_Click(sender As Object, e As EventArgs) Handles AreaCuadrado.Click
+        EvaluayHazOperacion()
+        Operacion = "c"
+    End Sub
+    Private Sub AreaRectangulo_Click(sender As Object, e As EventArgs) Handles AreaRectangulo.Click
+        EvaluayHazOperacion()
+        Operacion = "r"
+    End Sub
+    Private Sub AreaTriangulo_Click(sender As Object, e As EventArgs) Handles AreaTriangulo.Click
+        EvaluayHazOperacion()
+        Operacion = "t"
+    End Sub
+
+    Private Sub ButtonLimpiar_Click(sender As Object, e As EventArgs) Handles ButtonLimpiar.Click
+        TextResultado.Text = "0"
+        Valor2 = Nothing
+        ValorResultado = Nothing
+    End Sub
+    Public Sub EvaluayHazOperacion()
+        SePresionaOperador = True
+        Valor2 = Val(TextResultado.Text
+                     )
+        If ValorResultado IsNot Nothing Then
+            Select Case Operacion
+                Case "c"
+                    ValorResultado = ValorResultado * ValorResultado
+                Case "r"
+                    ValorResultado = ValorResultado * Valor2
+                Case "t"
+                    ValorResultado = (ValorResultado * Valor2) / 2
+
+            End Select
+            TextResultado.Text = ValorResultado
+        Else
+            ValorResultado = Valor2
+
+        End If
+    End Sub
+
+    Public Sub EvaluaRestriccionesParaConcatenar()
+        If SePresionaOperador = True Then
+            TextResultado.Text = ""
+            SePresionaOperador = False
+        ElseIf TextResultado.Text = "0" Then
+            TextResultado.Text = ""
+        End If
+    End Sub
+
+    Private Sub Buttoni_Click(sender As Object, e As EventArgs) Handles Buttoni.Click
+        EvaluayHazOperacion()
+        Operacion = ""
+    End Sub
+
+    Private Sub ButtonBack_Click(sender As Object, e As EventArgs) Handles ButtonBack.Click
+        Dim f2 As New Form2
+        f2.Show()
+        Me.Hide()
+    End Sub
+End Class
